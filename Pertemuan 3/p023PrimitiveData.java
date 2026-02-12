c60a003f307b8496f50f8820bdf914a7e31e0578
feniwputri <fnniidwii@gmail.com>
2026-02-12 15:29:07 +0700

    Create p023PrimitiveData.java

---
+++ Pertemuan 3/p023PrimitiveData.java
@@ -0,0 +1,32 @@
+public class p023PrimitiveData {
+
+public static void main(String[] args) {
+    
+    //Tipe data bilangan bulat
+    int umur = 20;
+    long populasi = 8000000000L;
+
+    //Tipe data bilangan desimal
+    float tinggi = 170.5f;
+    double ipk =  3.75;
+
+    //Tipe data karakter 
+    char nilaihuruf = 'A';
+
+    //Tipe data logika
+    boolean lulus = true;
+
+    //Menampilkan semua nilai variabe
+    System.out.println("Umur            : " + umur);
+    System.out.println("Populasi        : " + populasi);
+    System.out.println("Tinggi badan    : " + tinggi);
+    System.out.println("IPK             : " + ipk);
+    System.out.println("Nilai huruf     : " + nilaihuruf);
+    System.out.println("Status lulus    : " + lulus);
+
+}
+}
+// Nama : Feni Dwi Putri
+// NIM  : D1041241041
+// Tanggal : 12 Februari 2025
+
