b73aa9cecab71fe648c34d994a8554d030a1e779
feniwputri <fnniidwii@gmail.com>
2026-02-12 15:33:41 +0700

    Pertemuan 1

---
+++ Pertemuan 1/KodePertama.java
@@ -0,0 +1,9 @@
+public class KodePertama {
+    public static void main(String[] args) {
+        System.out.println("Halo, Pemrograman Beriontasi Objek!");
+        System.out.println("Ini adalah program Java pertama saya");
+    }
+}
+// Nama : Feni Dwi Putri
+// NIM  : D1041241041
+// Tanggal : 5 Februari 2025
---
+++ Pertemuan 1/KodePertama_Latihan.java
@@ -0,0 +1,11 @@
+public class KodePertama_Latihan {
+    public static void main(String[] args) {
+        System.out.println("Nama : Feni Dwi Putri");
+        System.out.println("NIM : D1041241041");
+        System.out.println("Semester : 4");
+        System.out.println("Prodi : Informatika");
+    }
+}
+// Nama : Feni Dwi Putri
+// NIM  : D1041241041
+// Tanggal : 5 Februari 2025
